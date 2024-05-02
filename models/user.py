8e5e603ed1a95b855b13d53ccfa37ebaf0bf9b62
Samar <bantmasr21@gmail.com>
2024-05-02 03:07:16 +0300

    added

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy import Column, String
-#from sqlalchemy import relationship
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -12,5 +12,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-
-
+    places = relationship('Place', backref='user', cascade='all, delete-orphan')
+    reviews = relationship('Review', backref='user', cascade='all, delete-orphan')
