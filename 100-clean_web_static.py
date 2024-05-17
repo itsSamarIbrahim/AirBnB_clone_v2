d95a2d56f792e561455fc6d05d640a5f4d13b84b
Samar -- <bantmasr21@gmail.com>
2024-05-16 17:08:50 -0700

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,33 +1,91 @@
 #!/usr/bin/python3
-"""
-A Fabric script (based on the file 3-deploy_web_static.py) that deletes
-out-of-date archives, using the function do_clean
-"""
-from os import listdir
-from fabric.api import env, lcd, local, run, cd
+'''deletes out-of-date archives, using the function do_clean'''
+import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
 
 
 env.hosts = ['54.82.208.14', '54.161.250.120']
 
 
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
+
+
+def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
+    if not os.path.exists(archive_path):
+        return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
+
+
+def deploy():
+    """Archives and deploys the static files to the host servers.
+    """
+    archive_path = do_pack()
+    return do_deploy(archive_path) if archive_path else False
+
+
 def do_clean(number=0):
+    """Deletes out-of-date archives of the static files.
+    Args:
+        number (Any): The number of archives to keep.
     """
-    Parameters:
-    number (int): The number of archives to retain.
-
-    If the value of 'number' is 0 or 1, only the latest archive is retained.
-    If the value is 2, the two most recent archives are kept, and so forth.
-    """
-    number 1 if int(number) == 0 else int(number)
-    archives = sorted(os.listdir("versions"))
-
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}"/format(archive)) for archive in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = \
-            [archive for archive in archives if "web_static_" in archive]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(archive)) for archive in archives]
+    archives = os.listdir('versions/')
+    archives.sort(reverse=True)
+    start = int(number)
+    if not start:
+        start += 1
+    if start < len(archives):
+        archives = archives[start:]
+    else:
+        archives = []
+    for archive in archives:
+        os.unlink('versions/{}'.format(archive))
+    cmd_parts = [
+        "rm -rf $(",
+        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
+        " '/data/web_static/releases/web_static_.*'",
+        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
+    ]
+    run(''.join(cmd_parts))
