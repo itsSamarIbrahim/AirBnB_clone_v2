49680e80dc256c898a19a337fe05b28110a1de8a
Samar <bantmasr21@gmail.com>
2024-06-02 16:06:42 -0700

    task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,11 +3,12 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
-from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+import shlex
 
 
 class FileStorage:
@@ -17,70 +18,48 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        dic = {}
         if cls:
-            if isinstance(cls, str):
-                cls = globals().get(cls)
-            if cls and issubclass(cls, BaseModel):
-                cls_dict = {k: v for k, v in self.__objects.items()
-                            if isinstance(v, cls)}
-                return cls_dict
-
-        return FileStorage.__objects
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        dictionary = {}
+        for key, value in self.__objects.items():
+            dictionary[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding='UTF-8') as f:
+            json.dump(dictionary, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    class_name = val['__class__']
-                    if class_name in classes:
-                        # self.all()[key] = classes[val['__class__']](**val)
-                        self.all()[key] = classes[class_name](**val)
-                    else:
-                        print(f"Unknown class '{class_name}' \
-                                encountered in JSON data.")
+            with open(self.__file_path, 'r', encoding='UTF-8') as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
         except FileNotFoundError:
-            pass
-        except json.decoder.JSONDecodeError:
             pass
 
     def delete(self, obj=None):
-        """
-        deletes obj from __objects if it’s inside - if obj is equal to None,
-        the method should not do anything
-        """
-        if obj is None:
-            return
-        obj_to_delete = f"{obj.__class__.__name__}.{obj.id}"
-
-        try:
-            del FileStorage.__objects[obj_to_delete]
-        except AttributeError:
-            pass
-        except KeyboardInterrupt:
-            pass
+        """ delete obj from __objects if it’s inside """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
 
     def close(self):
-        """reload"""
+        """ reload """
         self.reload()
