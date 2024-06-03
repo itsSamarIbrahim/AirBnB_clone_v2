bd4ef8865097cb423e82c38f0d33e932985d750b
Samar <bantmasr21@gmail.com>
2024-06-02 18:08:13 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,45 +1,27 @@
 #!/usr/bin/python3
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+    
 """
-A script that starts a Flask web application:
-
-    Your web application must be listening on 0.0.0.0, port 5000
-    You must use storage for fetching data from the storage engine (FileStorage
-      or DBStorage) => from models import storage and storage.all(...)
-    After each request you must remove the current SQLAlchemy Session:
-        Declare a method to handle @app.teardown_appcontext
-        Call in this method storage.close()
-    Routes:
-        /states_list: display a HTML page: (inside the tag BODY)
-            H1 tag: “States”
-            UL tag: with the list of all State objects present in DBStorage
-                    sorted by name (A->Z) tip
-            LI tag: description of one State: <state.id>: <B><state.name></B>
-    Import this 7-dump to have some data
-    You must use the option strict_slashes=False in your route definition
-"""
+from models import storage
 from flask import Flask
 from flask import render_template
-from models import storage
-from models import *
-
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
     """
-    Displays a HTML page with a list of all State objects present in DBStorage
-    """
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    Closes the storage on teardown
-    """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
